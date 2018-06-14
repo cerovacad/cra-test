d397d776cab86cacfa36140480a444a4a1b2793c
cerovacad <cerovacad@gmail.com>
2018-06-14 15:25:54 +0200

    components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import Router from './router/Router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Router />, document.getElementById('root'));
+
